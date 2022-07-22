775da15aa5bff19d4dbc208aefe9159a4fddeb9d
김모세 <msu077@naver.com>
2022-07-22 12:47:53 +0900

    teamP 3-3

---
+++ src/main/java/com/team/pj/addController.java
@@ -0,0 +1,15 @@
+package com.team.pj;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class addController {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	// go
+
+}
