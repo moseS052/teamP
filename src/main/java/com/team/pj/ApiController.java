d818fb027e4e0041c2f1dcd5e96e1294d7431165
김모세 <msu077@naver.com>
2022-07-22 11:49:24 +0900

    teamP 3-1

--- src/main/java/com/team/pj/ApiController.java
+++ src/main/java/com/team/pj/ApiController.java
@@ -39,7 +39,7 @@ import org.w3c.dom.NodeList;
 import ch.qos.logback.core.net.SyslogOutputStream;
 
 @Controller
-public class addController {
+public class ApiController {
 	
 	@Autowired
 	private SqlSession sqlSession;
