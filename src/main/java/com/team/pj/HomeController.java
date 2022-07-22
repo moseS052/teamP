b560448d697fef6a8e6e08173b4168230cb1fda5
김모세 <msu077@naver.com>
2022-07-22 11:12:10 +0900

    teamP 2-3-3

--- src/main/java/com/team/pj/HomeController.java
+++ src/main/java/com/team/pj/HomeController.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.session.SqlSession;
 import org.json.simple.JSONArray;
@@ -17,6 +18,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -31,13 +33,114 @@ public class HomeController {
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
-	//°ÇµéÁö ¸¶¼¼¿ä
+	//ï¿½Çµï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
 
 //	@RequestMapping(value = "/", method = RequestMethod.GET)
 //	public String home() {
 //		
 //		return "home";
 //	}
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(HttpServletRequest req, Model model) {
+		HttpSession session=req.getSession();
+		if(session.getAttribute("newuser")==null) { //ë¡œê·¸ì¸ ì „
+			model.addAttribute("userinfo","");
+			
+
+		}else { //ë¡œê·¸ì¸ ì„±ê³µ í›„
+			model.addAttribute("userinfo",session.getAttribute("newuser"));
+			
+		}
+		return "home";
+		
+	}
+	//ë¡œê·¸ì¸
+	@RequestMapping(value="/login", method=RequestMethod.GET)
+	public String doLogin() {
+		return "login";
+	}
+	//íšŒì›ê°€ì…jsp
+	@RequestMapping(value="/signup", method=RequestMethod.GET)
+	public String doSignup() {
+		return "signup";
+	}
+	@RequestMapping(value="/user_check", method=RequestMethod.POST)
+	public String doCheck(HttpServletRequest req,Model model) {
+		HttpSession session=req.getSession();
+		iteamP p=sqlSession.getMapper(iteamP.class);
+		ArrayList<memberDTO> mlist=p.listMember();
+		model.addAttribute("signlist",mlist);
+		int n=p.count(req.getParameter("id"),Integer.parseInt(req.getParameter("pw")));
+		model.addAttribute("cnt",n);
+		System.out.println("cnt="+n);
+		
+		String user_id=req.getParameter("id");
+		String password=req.getParameter("pw");
+		
+		if(n==0) { //ë¡œê·¸ì¸ ì‹¤íŒ¨			
+			session.setAttribute("newuser",null);
+			
+		}else if(n!=1) { //ì—ëŸ¬
+			
+		}else {	//ë¡œê·¸ì¸ ì„±ê³µ
+			session.setAttribute("newuser",user_id);			
+		}
+		return "redirect:/";
+	}
+	//ë¡œê·¸ì•„ì›ƒ
+	@RequestMapping("/logout")
+	public String doLogout(HttpServletRequest req) {
+		HttpSession session=req.getSession();
+		session.invalidate();
+		return "redirect:/";
+	}
+	//íšŒì›ê°€ì…
+	@ResponseBody
+	@RequestMapping(value="/sign", method=RequestMethod.POST, produces="application/text;charset=utf8")
+	public String doSign(@RequestParam("id") String id,@RequestParam("pw") String pw,
+			@RequestParam("name") String name,
+			@RequestParam("nick") String nick,@RequestParam("phone") String phone,
+			@RequestParam("gender") String gender,@RequestParam("mail") String mail,
+			@RequestParam("birth") String birth) {
+		iteamP p=sqlSession.getMapper(iteamP.class);
+		System.out.println("id="+id);
+		p.insert(id,pw,name,nick,phone,gender,mail,birth);
+		
+		return "";
+	}
+	//íšŒì›ê°€ì…ì‹œ ì¬ëŠ¥ì²´í¬í…Œì´ë¸”ì—ë„ ì¶”ê°€
+	@ResponseBody
+	@RequestMapping(value="/talent", method=RequestMethod.POST, produces="application/text;charset=utf8")
+	public String doTalent(@RequestParam("id") String id, @RequestParam("t_no") String t_no) {
+		iteamP p=sqlSession.getMapper(iteamP.class);
+		String[] a = t_no.split(" ");
+		for(int i=0; i<a.length;i++) {
+			p.talent(p.getM_no1(id),Integer.parseInt(a[i]));
+			System.out.println("t_no="+a[i]);
+		}
+		
+		
+		return "";
+	}
+	//ì•„ì´ë”” ì¤‘ë³µì²´í¬
+	@ResponseBody
+	@RequestMapping(value="/idcheck", method=RequestMethod.GET, produces="application/text;charset=utf8")
+	public String doIdcheck(@RequestParam("id") String id, Model model) {
+		iteamP p=sqlSession.getMapper(iteamP.class);
+		int idcnt=p.id(id);
+		System.out.println("idcnt="+idcnt);
+		return Integer.toString(idcnt);		
+	}
+	//ë‹‰ë„¤ì„ ì¤‘ë³µì²´í¬
+	@ResponseBody
+	@RequestMapping(value="/nickcheck", method=RequestMethod.GET, produces="application/text;charset=utf8")
+	public String doNickcheck(@RequestParam("nick") String nick, Model model) {
+		iteamP p=sqlSession.getMapper(iteamP.class);
+		int nickcnt=p.nick(nick);
+		System.out.println("idcnt="+nickcnt);
+		return Integer.toString(nickcnt);		
+	}
+	
 	
 	@RequestMapping(value = "/ad", method = RequestMethod.GET)
 	public String ad() {
@@ -45,29 +148,29 @@ public class HomeController {
 		return "addjsp";
 	}
 
-	//¾Æ¹ÙÅ¸ ´­·¶À» ¶§ È¸¿øÁ¤º¸ ¶ß´Â °÷
+	//ï¿½Æ¹ï¿½Å¸ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ È¸ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ß´ï¿½ ï¿½ï¿½
 	@RequestMapping(value = "/meminfo", method = RequestMethod.GET)
 	public String meminfo() {
-		//¼¼¼Ç
+		//ï¿½ï¿½ï¿½ï¿½
 		return "memf";
 	}
 	
-	//ÂÊÁöº¸³»±â ´­·¶À» ¶§(»çÀÌÆ® ¶ß±â)
+	//ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½(ï¿½ï¿½ï¿½ï¿½Æ® ï¿½ß±ï¿½)
 	@RequestMapping(value = "/note", method = RequestMethod.GET)
 	public String note(HttpServletRequest req, Model model) {
-		model.addAttribute("mno",req.getParameter("m_no")); //¼¼¼ÇÇÊ¿ä
+		model.addAttribute("mno",req.getParameter("m_no")); //ï¿½ï¿½ï¿½ï¿½ï¿½Ê¿ï¿½
 		model.addAttribute("mpano",req.getParameter("m_pa_no"));
 		return "note";
 	}
 	
-	//ÂÊÁö ¸®½ºÆ®¾÷
+	//ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½Æ®ï¿½ï¿½
 	@ResponseBody
 	@RequestMapping(value="/noteUp",produces="application/text;charset=utf8")
 	public String noteUp(HttpServletRequest req) {
 		int m_no=Integer.parseInt(req.getParameter("m_no"));
 		int m_pa_no=Integer.parseInt(req.getParameter("m_pa_no"));
 		iteamP ip=sqlSession.getMapper(iteamP.class);
-		ArrayList<noteVO> arno=ip.getNoteCon(m_no,m_pa_no);  //¼¼¼Ç
+		ArrayList<noteVO> arno=ip.getNoteCon(m_no,m_pa_no);  //ï¿½ï¿½ï¿½ï¿½
 		JSONArray ja=new JSONArray();
 		for(int i=0;i<arno.size();i++) {
 			noteVO nvo=arno.get(i);
@@ -82,14 +185,14 @@ public class HomeController {
 		return ja.toJSONString();
 	}
 	
-	//ÂÊÁö º¸³»´Â ÄÁÆ®·Ñ·¯
+	//ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½Æ®ï¿½Ñ·ï¿½
 	@ResponseBody
 	@RequestMapping(value="/noteSend",produces="application/text;charset=utf8")
 	public String noteSend(HttpServletRequest req) {
 		iteamP ip=sqlSession.getMapper(iteamP.class);
 		String con=req.getParameter("n_con");
 		int you=Integer.parseInt(req.getParameter("youseq"));
-		int me=1; //¼¼¼Ç¸ÎÀ¸¸é ¼¼¼Ç¾µ °Í
+		int me=1; //ï¿½ï¿½ï¿½Ç¸ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½Ç¾ï¿½ ï¿½ï¿½
 		ip.noteSend(me, you, con);
 		return "";
 	}
